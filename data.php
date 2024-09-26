118291679586199b38c3f523c87ba0a16b413a05
otaviotedesco <otavio.tedesco@gmail.com>
2024-09-26 14:03:30 +0100

    Commit 2

---
+++ data.php
@@ -0,0 +1,12 @@
+<?php
+
+date_default_timezone_set('Europe/Lisbon');
+$data = date('d/m/y H:i:s');
+echo $data;
+echo "<br>";
+
+
+echo date ('D,d/M/Y H:i:s');
+
+
+?>
---
+++ refrigerante.php
@@ -0,0 +1,12 @@
+<?php
+
+$refrigerante = array('Coca-Cola', 'Pepsi', 'Guaraná', 'Fanta', 'Sukita', 'Sprite');
+
+echo $refrigerante[0] . '<br>';
+echo $refrigerante[1] . '<br>';
+
+print_r($refrigerante [2]);
+
+
+
+?>
