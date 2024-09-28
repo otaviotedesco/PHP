96e89f34ceff0fa3201c2615aef318a07b5eb1ba
otaviotedesco <otavio.tedesco@gmail.com>
2024-09-28 09:43:02 +0100

    Create soma-numeros.php

---
+++ soma-numeros.php
@@ -0,0 +1,18 @@
+<?php
+
+echo ("Dado um número armazenado na variável N, calcule a soma S de todos os números naturais entre 1 e n. <br>");
+
+$n = 10;
+$s = 0;
+
+for ($i = 1; $i <= $n; $i++) {
+    
+    echo $s = $s + $i; "<br>";
+    echo "<br>";
+
+}
+
+
+
+
+?>
